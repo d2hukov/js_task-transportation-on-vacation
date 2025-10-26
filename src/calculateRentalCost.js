263cd93f2fd62e2a2a600c9d19c3b2f421f1d7c0
mist1que <allouser020220@gmail.com>
2025-10-26 19:39:11 +0200

    Refactor: replace magic numbers with constants and use early returns

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,25 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let cost = days * 40;
+  const DAILY_RATE = 40;
+  const DISCOUNT_FOR_3_DAYS = 20;
+  const DISCOUNT_FOR_7_DAYS = 50;
+  const DAYS_FOR_3_DAY_DISCOUNT = 3;
+  const DAYS_FOR_7_DAY_DISCOUNT = 7;
 
-  if (days >= 7) {
-    cost = cost - 50;
-  } else if (days >= 3) {
-    cost = cost - 20;
+  const baseCost = days * DAILY_RATE;
+
+  if (days >= DAYS_FOR_7_DAY_DISCOUNT) {
+    return baseCost - DISCOUNT_FOR_7_DAYS;
   }
 
-  return cost;
+  if (days >= DAYS_FOR_3_DAY_DISCOUNT) {
+    return baseCost - DISCOUNT_FOR_3_DAYS;
+  }
+
+  return baseCost;
 }
+
+module.exports = calculateRentalCost;
 
 module.exports = calculateRentalCost;
